e4c760acdcd7af2c3d18211890c2c825b3bb5ac7
Pafcholini <nadyaivanova14@gmail.com>
2016-03-03 02:30:29 +0100

    cpufreq: add governors
    *InteractiveX
    *ConservativeX
    
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -467,6 +467,9 @@ extern struct cpufreq_governor cpufreq_gov_bluactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE_PRO)
 extern struct cpufreq_governor cpufreq_gov_interactive_pro;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive_pro)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 /*********************************************************************
