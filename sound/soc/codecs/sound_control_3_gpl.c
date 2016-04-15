2a7f0d941b48758c697d0451626a9fba5e0533af
placiano <placiano80@gmail.com>
2016-04-15 19:30:28 +0200

    sound_control: Lock Output Gain active by default
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -31,7 +31,7 @@
 extern struct snd_soc_codec *fauxsound_codec_ptr;
 extern int wcd9xxx_hw_revision;
 
-static int snd_ctrl_locked = 0;
+static int snd_ctrl_locked = 1;
 static int snd_rec_ctrl_locked = 0;
 
 unsigned int tomtom_read(struct snd_soc_codec *codec, unsigned int reg);
